f3fb77b042e188fe7a431b229964df7e35d72d34
BrianAdams <brian@babastik.com>
2015-10-25 02:12:26 -0700

    Linked in the settings and digs shared controls

--- src/plugins/theme_r2/index.js
+++ src/plugins/theme_r2/index.js
@@ -2,7 +2,7 @@ function theme_r2(name, deps) {
   console.log('This is where theme_r2 plugin code would execute in the node process.');
 
   this.plugin={
-    name: "themes_r2",
+    name: "theme_r2",
     type: "theme"
   };
 }

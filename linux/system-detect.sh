b790b306b2db288c3aed8ab100a66b56a1fbd09d
Charles Cross <charles@openrov.com>
2016-04-13 02:09:45 +0000

    Last change was made on a broken controller board, so the fix was wrong. Changing back to correct values

--- linux/system-detect.sh
+++ linux/system-detect.sh
@@ -6,10 +6,8 @@ canTalkToATMEGAviaSPI(){
     avrdude -P /dev/spidev1.0 -c linuxspi -vv -p m2560 > /dev/null 2>&1
     if [ $? -ne 0 ] 
     then
-	echo yay
         return 1
     else
-	echo boo
         return 0
     fi
     
@@ -19,12 +17,10 @@ export ROV_BOARD=custom
 
 
 canTalkToATMEGAviaSPI;
-if [ "$?" -eq 1 ];
+if [ "$?" -eq 0 ];
 then
-	echo cb
     export ROV_BOARD=board25
 else
-	echo cape
     export ROV_BOARD=cape
 fi
 

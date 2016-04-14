e073ed37db0914f9eb8a5b3ae5d1f710331d1445
badevguru <brian@babastik.com>
2016-04-14 01:33:36 -0700

    Builtin Recording and Exporting of MP4 video (#44)
    * Video data will be recorded when the blackbox recording feature is turned on.
    * Because the MP4 data is already compressed, the saving of the data does not impact CPU/battery. No transcoding!
    * New /data url for downloading, and eventually managing saved data

--- src/plugins/blackbox/public/js/blackbox.js
+++ src/plugins/blackbox/public/js/blackbox.js
@@ -1,19 +1,21 @@
 (function (window, document, jQuery) {
   'use strict';
   var plugins = namespace('plugins');
+  //jQuery.getScript('/components/dexie/dist/latest/Dexie.js');
 
+  var head = document.getElementsByTagName("head")[0];
+  var js = document.createElement("script");
+  js.type = "text/javascript";
+  js.src = '/components/dexie/dist/latest/Dexie.js';
+  head.appendChild(js);
 
   var Blackbox = function Blackbox(cockpit) {
     console.log('Loading Blackbox plugin.');
     this.cockpit = cockpit;
     this.recording = false;
     this.idb;
+    this.sessionID = this.newSession();
 
-    this.idb = this.defineDB(); //Readies the DB, ensures schema is consistent
-    this.idb.on('error', function (err) {
-        // Catch all uncatched DB-related errors and exceptions
-        console.error(err);
-    });
   };
 
   plugins.Blackbox = Blackbox;
@@ -33,6 +35,19 @@
   Blackbox.prototype.listen = function listen() {
     var self = this;
 
+    if (window.Dexie===undefined){
+//      $.getScript('/components/dexie/dist/latest/Dexie.js',function(){
+//        self.listen();
+//      });
+      setTimeout(function(){self.listen()},1000);
+      return;
+    }
+    this.idb = this.defineDB(); //Readies the DB, ensures schema is consistent
+    this.idb.on('error', function (err) {
+        // Catch all uncatched DB-related errors and exceptions
+        console.error(err);
+    });
+
     this.cockpit.rov.on('plugin.navigationData.data', function (data) {
       if (!jQuery.isEmptyObject(data)) {
         self.logNavData(data);
@@ -43,9 +58,12 @@
         self.logStatusData(data);
       }
     });
-
+    this.cockpit.on('x-h264-video.data', function (data) {
+        self.logMP4Video(data);
+    });
     this.cockpit.on('plugin-blackbox-export', function(options){
       self.exportData(options);
+      self.exportVideo(options);
     });
 
     this.cockpit.on('plugin-blackbox-recording-start', function(){
@@ -56,14 +74,47 @@
       self.stopRecording();
     });
 
+    this.cockpit.on('plugin-blackbox-get-sessions', function(callback){
+      self.recordedSessions(callback);
+    });
+
     this.cockpit.on('plugin-blackbox-recording?', function(fn){
       if(typeof(fn) === 'function'){
         fn(self.recording);
       }
     });
 
+    this.recordedSessions(function(sessions){
+        self.cockpit.emit('plugin-blackbox-sessions',sessions)
+    });
 
   };
+
+  var sessionIDRecorded = false;
+  Blackbox.prototype.newSession = function newSession(){
+     return generateUUID();
+  }
+
+  //TODO: Add sessions collection that each unique session is placed
+  var _recordedSessions = function recordedSessions(idb,callback){
+    idb.sessions.toArray(function(data){
+      callback(data);
+    });
+  };
+
+  Blackbox.prototype.recordedSessions = function recordedSessions(callback){
+    if (!this.idb.isOpen()) {
+      this.idb.open()
+        .catch(function (error) {
+          console.error(error);
+        });
+      _recordedSessions(this.idb,callback);
+      this.idb.close();
+    } else {
+      _recordedSessions(this.idb,callback);
+    }
+
+  }
 
   Blackbox.prototype.toggleRecording = function toggleRecording() {
     if (this.recording){
@@ -75,9 +126,17 @@
 
   Blackbox.prototype.startRecording = function startRecording() {
     if (!this.recording) {
+      var self=this;
       console.log('Recording Telemetry');
       var blackbox = this;
       this.idb.open();
+      if(!sessionIDRecorded){
+        this.idb.sessions.add({sessionID:this.sessionID,timestamp:Date.now()});
+        this.recordedSessions(function(sessions){
+          self.cockpit.emit('plugin-blackbox-sessions',sessions)
+        });
+        sessionIDRecorded=true;
+      }
       this.recording = true;
       this.cockpit.emit('plugin-blackbox-recording-status',true);
     }
@@ -91,12 +150,36 @@
       this.cockpit.emit('plugin-blackbox-recording-status',false);
     }
   };
+  var initFrame=null;
+
+  window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder ||
+                       window.MozBlobBuilder;
+
+  Blackbox.prototype.logMP4Video = function logMP4Video(data) {
+    var self=this;
+    if (!this.recording) {
+      return;
+    }
+    if (initFrame==null){
+      this.cockpit.emit('request_Init_Segment', function(init) {
+        initFrame=init;
+        self.logMP4Video.call(self,init);
+      });
+    } else {
+    //var myblob = new Blob([data]);
+    this.idb.mp4.add({timestamp: Date.now(),sessionID:this.sessionID,data:data})
+      .catch(function (error) {
+        console.error(error);
+      });
+    }
+  };
 
   Blackbox.prototype.logNavData = function logNavData(navdata) {
     if (!this.recording) {
       return;
     }
     navdata.timestamp = Date.now();
+    navdata.sessionID= this.sessionID;
     this.idb.navdata.add(navdata)
       .catch(function (error) {
         console.error(error);
@@ -108,6 +191,7 @@
       return;
     }
     statusdata.timestamp = Date.now();
+    statusdata.sessionID= this.sessionID;
     this.idb.telemetry.add(statusdata)
       .catch(function (error) {
         console.error(error);
@@ -117,9 +201,11 @@
   Blackbox.prototype.defineDB = function defineDB(callback){
     //Instructions to upgrade: https://github.com/dfahlander/Dexie.js/wiki/Design
     var idb = new Dexie("openrov-blackbox2");
-    idb.version(1).stores({
-        navdata: 'timestamp',
-        telemetry: 'timestamp',
+    idb.version(3).stores({
+        navdata: 'id++,timestamp,sessionID',
+        telemetry: 'id++,timestamp,sessionID',
+        mp4: 'id++,timestamp,sessionID',
+        sessions: 'timestamp,sessionID'
     });
     return idb;
   }
@@ -135,7 +221,7 @@
 
     for(var i in cols){
       options.collection = cols[i];
-      if (!this.recording) {
+      if ((!this.idb.isOpen())) {
         this.idb.open()
           .catch(function (error) {
             console.error(error);
@@ -144,6 +230,31 @@
         this.idb.close();
       } else {
         this._exportData(options);
+      }
+    }
+
+  };
+
+  Blackbox.prototype.exportVideo = function exportVideo(options){
+    var cols;
+
+//    if(options.collection === "*"){
+      cols = ['mp4'];
+//    } else {
+//      cols = [options.collection];
+//    }
+
+    for(var i in cols){
+      options.collection = cols[i];
+      if (!this.idb.isOpen()) {
+        this.idb.open()
+          .catch(function (error) {
+            console.error(error);
+          });
+        this._exportVideo(options);
+        this.idb.close();
+      } else {
+        this._exportVideo(options);
       }
     }
 
@@ -174,7 +285,7 @@
       fakeClick(link);
     };
 
-    this.idb[options.collection].toArray(function(name,dump){
+    this.idb[options.collection].where("sessionID").equalsIgnoreCase(options.sessionID).toArray(function(name,dump){
       var serializedData;
       switch(options.format){
         case 'json': serializedData = JSON.stringify(dump);
@@ -184,10 +295,74 @@
         case 'csv':
         default: serializedData = new CSV(dump, {header: true}).encode(); //TODO:
       }
-      downloadInBrowser(serializedData,name+'.'+options.format);
+      downloadInBrowser(serializedData,name+'-'+options.sessionID+'.'+options.format);
     }.bind(null,options.collection));
 
   };
+
+  Blackbox.prototype._exportVideo = function _exportVideo(options,callback){
+
+    var fakeClick = function fakeClick(anchorObj) {
+      if (anchorObj.click) {
+        anchorObj.click();
+      } else if (document.createEvent) {
+        if (event.target !== anchorObj) {
+          var evt = document.createEvent('MouseEvents');
+          evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+          var allowDefault = anchorObj.dispatchEvent(evt);
+        }
+      }
+    };
+
+    var downloadInBrowser = function downloadInBrowser(data,name){
+      var blob = new Blob([data], { 'type': 'video/mp4' });
+      var link = document.createElement("A");
+      link.setAttribute('href', window.URL.createObjectURL(blob));
+      link.setAttribute('download',name);
+      //download="data.json"
+      //link.attr('href', window.URL.createObjectURL(blob));
+      document.body.appendChild(link);
+      fakeClick(link);
+    };
+
+
+    this.idb[options.collection].where("sessionID").equalsIgnoreCase(options.sessionID).toArray(function(name,dump){
+      var sizeofData = 0
+      var arrayOfData = dump.map(function(item){
+        var converted = new Uint8Array(item.data);
+        sizeofData+=converted.length;
+        return converted;
+      });
+      var result = new Uint8Array(sizeofData);
+      var tail = 0;
+      arrayOfData.forEach(function(item){
+        result.set(item,tail);
+        tail+=item.length;
+      });
+
+
+//      var result = Uint8Array.of.apply(this,arrayOfData);
+/*
+
+
+      var bufferlength=0;
+      var dataArray = dump.reduce(function(previous,current,index,array){
+        var b = new Uint8Array(current.data);
+        //var c = new Uint8Array(previous.length + b.length);
+        //c.set(previous);
+        if (bufferlength+b.length>previous.length){
+          var c =
+        }
+        previous.set(b, bufferlength);
+        bufferlength+=b.length
+        return c;
+      },new Uint8Array(2000000));
+*/
+      downloadInBrowser(result,name+'-'+options.sessionID+'.'+'mp4');
+    }.bind(null,options.collection));
+
+  };
+
 
   window.Cockpit.plugins.push(Blackbox);
 

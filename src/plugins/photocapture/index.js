ac2c6778a39d71730877424ab2a9fc334cb9120a
BrianAdams <brian@babastik.com>
2016-04-16 17:52:50 -0700

    Removed broken plugin breaking gamepad

--- src/plugins/photocapture/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  function PhotoCapture(name, deps) {
-    console.log('PhotoCapture plugin loaded');
-  }
-  module.exports = function (name, deps) {
-    return new PhotoCapture(name,deps);
-  };
-})();

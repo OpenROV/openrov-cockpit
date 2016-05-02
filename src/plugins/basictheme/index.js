f09927145754272c61d741162470fdc32f73e282
BrianAdams <brian@babastik.com>
2016-05-02 14:09:57 -0700

    code review feedback

---
+++ src/plugins/basictheme/index.js
@@ -0,0 +1,16 @@
+function basictheme( name, deps )
+{
+	console.log( 'Basic UI plugin loaded.' );
+
+	this.plugin =
+	{
+		name: "basictheme",
+		type: "theme"
+	};
+
+}
+
+module.exports = function( name,deps )
+{
+  return new basictheme( name, deps );
+};

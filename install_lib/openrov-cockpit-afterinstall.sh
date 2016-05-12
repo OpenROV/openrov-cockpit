1315bff0eec1e9b12b6614ee10c6c7ceeb6123bf
BrianAdams <brian@babastik.com>
2016-05-12 02:51:04 -0700

    Merge remote-tracking branch 'OpenROV/master' into cleanup
    # Conflicts:
    #	install_lib/openrov-cockpit-afterinstall.sh

--- install_lib/openrov-cockpit-afterinstall.sh
+++ install_lib/openrov-cockpit-afterinstall.sh
@@ -12,6 +12,7 @@ chmod +x /opt/openrov/cockpit/linux/rc.local
 chown -R rov /opt/openrov/cockpit
 chgrp -R admin /opt/openrov/cockpit
 
+
 # setup reset and uart for non black BB
 cp /etc/rc.local /etc/rc.local_orig
 cat > /etc/rc.local << __EOF__

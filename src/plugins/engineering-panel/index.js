9e7e2086e138f63665f087a26cdb30bc93a41ce5
Joakim Karlsson <jocke@covedesign.se>
2016-06-03 18:38:11 -0700

    Engineering screen
    First version of the Engineering screen.

--- src/plugins/engineering-panel/index.js
+++ src/plugins/engineering-panel/index.js
@@ -1,15 +1,27 @@
 function engineeringPanel(name, deps) {
-  console.log('This is where the engineering panel plugin code would execute in the node process.');
+	console.log('This is where the engineering panel plugin code would execute in the node process.');
 
-  //instance variables
-  this.deps = deps; //hold a reference to the plugin dependencies if you are going to use them
-  this.globalEventLoop = deps.globalEventLoop; //explicitlly calling out the rov eventemitter
-  this.cockpit = deps.cockpit; //explicitly calling out cockpit eventemitter
+	//instance variables
+	this.deps = deps; //hold a reference to the plugin dependencies if you are going to use them
+	this.globalEventLoop = deps.globalEventLoop; //explicitlly calling out the rov eventemitter
+	this.cockpit = deps.cockpit; //explicitly calling out cockpit eventemitter
+	this.statusdata = {};
 }
 
 // Start is executed after all plugins have loaded. Activate listeners here.
 engineeringPanel.prototype.start = function start(){
-  var self = this; //set closure state variable for use in functions
+  var self = this; 
+
+	self.deps.globalEventLoop.on( 'physicalInterface.status', function(data){
+	    for (var i in data) {
+			if (i === 'cmd'){
+				if (data[i].indexOf('ping')>=0) continue;
+			}
+			self.statusdata[i] = data[i];
+		}
+		self.deps.cockpit.emit('plugin.engineering.data', self.statusdata);
+	});
+
 }
 
 module.exports = function (name, deps) {

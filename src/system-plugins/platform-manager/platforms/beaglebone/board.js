6f73bf8cfe8742238bba9b99f9751cfe469ff369
Charles Cross <charles@openrov.com>
2016-07-01 16:23:18 -0700

    Updated bbb board interface loader

--- src/system-plugins/platform-manager/platforms/beaglebone/board.js
+++ src/system-plugins/platform-manager/platforms/beaglebone/board.js
@@ -1,51 +1,43 @@
-var Q 		= require("q");
-var fs 		= require("fs");
+var Promise = require( "bluebird" );
+var fs 		= Promise.promisifyAll( require("fs") );
 var path	= require('path');
-var Parser 	= require("binary-parser").Parser;
 
-var fopen		= Q.denodeify( fs.open );
-var read 		= Q.denodeify( fs.read );
-var readFile	= Q.denodeify( fs.readFile );
-
-var ComposeInterface = function( platform )
+var BoardInterface = function()
 {
-	return LoadBoardInfo( platform.mcu )
-			.then( LoadPinMap )
-			.then( LoadInterface )
-			.then( function( board )
-			{
-				// Success
-				return platform;
-			} )	
-			.catch( function( err )
-			{
-				console.log( "Err loading board info: " + err );
-				
-				// Fail, but return anyway
-				return platform;
-			} );
+	
 };
 
-var LoadBoardInfo = function( board ) 
+BoardInterface.prototype.Compose = function( platform )
+{	
+	// Temporary container used for cpu detection and info loading
+	var board =
+	{
+		targetBoard: platform.board
+	};
+	
+	var self = this;
+	
+	return self.LoadInfo( board )
+			.then( self.LoadPinMap )
+			.then( self.LoadInterface );
+};
+
+BoardInterface.prototype.LoadInfo = function( board ) 
 {
-	return Q.fcall( function()
+	board.info = {};
+	
+	return fs.readFileAsync( path.resolve( "/opt/openrov/system/etc/2xBoardInfo.json" ) )
+			.then( JSON.parse )
+			.then( function( info )
 			{
-				// Check SPI communication. If good, we're happy!
-				
-				board.info = 
-				{
-					productId: 0,
-					serial: 0
-				};
-				
+				board.info = info;
+				return board;
 			} );
-			
-			// TODO: Check eeprom as well. If it exists, we are a 2.8 specifically
 }
 
-var LoadPinMap = function( board )
+BoardInterface.prototype.LoadPinMap = function( board )
 {
-	return readFile( path.resolve(__dirname, "boards/" + board.info.productId + "/pinmap.json" ) )
+	return fs.readFileAsync( path.resolve( __dirname, "boards/" + board.info.productId + "/pinmap.json" ) )
 			.then( JSON.parse )
 			.then( function( json )
 			{
@@ -59,12 +51,10 @@ var LoadPinMap = function( board )
 			} );
 }
 
-var LoadInterface = function( board )
+BoardInterface.prototype.LoadInterface = function( board )
 {
-	// Load functions for the board interface
-	require( "./boards/" + board.info.productId + "/setup.js" )( board );
-	
+	require( "./boards/" + board.info.productId + "/setup.js" )( board.targetBoard );
 	return board;
 };
 
-module.exports = ComposeInterface;
+module.exports = new BoardInterface();

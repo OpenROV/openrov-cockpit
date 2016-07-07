ba61a3e55b10919db52cdecc1818cb90bda385d4
Charles Cross <charles@openrov.com>
2016-07-07 12:13:30 -0700

    Added post install line to update the overlays in config.txt

--- src/system-plugins/platform-manager/platforms/raspberrypi/install/scripts/post-install.sh
+++ src/system-plugins/platform-manager/platforms/raspberrypi/install/scripts/post-install.sh
@@ -4,3 +4,7 @@ set -e
 
 # Create soft link for nginx locations
 ln -s /opt/openrov/system/etc/nginx.location /etc/nginx/locations-enabled/cockpit.conf
+
+# Add dtoverlay line to config.txt
+echo "" >> /boot/config.txt
+echo "dtoverlay=openrov-eeprom" >> /boot/config.txt

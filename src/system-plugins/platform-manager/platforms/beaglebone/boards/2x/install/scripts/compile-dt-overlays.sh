85cc01e5f00b145afcfe9696d1a3510bae6cb4da
Charles Cross <charles@openrov.com>
2016-07-14 18:19:03 -0700

    Removed reset dtc compile

--- src/system-plugins/platform-manager/platforms/beaglebone/boards/2x/install/scripts/compile-dt-overlays.sh
+++ src/system-plugins/platform-manager/platforms/beaglebone/boards/2x/install/scripts/compile-dt-overlays.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -xe
 
-dtc -O dtb -o ./files/dt-overlays/OPENROV-RESET-00A0.dtbo -b 0 -@ ./files/dt-overlays/OPENROV-RESET-00A0.dts
 dtc -O dtb -o ./files/dt-overlays/OROV-CB25-00A0.dtbo -b 0 -@ ./files/dt-overlays/OROV-CB25-00A0.dts

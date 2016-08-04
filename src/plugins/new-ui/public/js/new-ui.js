505592defd7b39002742832fbc5dc64c2a7d1862
badevguru <brian@babastik.com>
2016-08-04 03:40:10 -0700

    New ui reactive (#115)
    * Improved the reactive nature of the new-ui theme for low-res displays
    
    * basic working collapsed nav

--- src/plugins/new-ui/public/js/new-ui.js
+++ src/plugins/new-ui/public/js/new-ui.js
@@ -8,7 +8,54 @@
     this.cockpit = cockpit;
     this.name = 'new-ui';   // for the settings
     this.viewName = 'New UI'; // for the UI
+
+    this.WhenVisible('#time-item',function(){
+      $('#time-item').css('visibility','visible');
+      $('#time-item-collapsed').hide();
+    },function(){
+      $('#time-item').css('visibility','hidden');
+      $('#time-item-collapsed').show();
+    })
+
+    this.WhenVisible('#power-item',function(){
+      $('#power-item').css('visibility','visible');
+      $('#power-item-collapsed').hide();
+    },function(){
+      $('#power-item').css('visibility','hidden');
+      $('#power-item-collapsed').show();
+    })    
+
   };
+
+  plugins.NewUI.prototype.WhenVisible=function(element,visibleCallback,obstructedCallback){
+   var fn = function(){
+      var win = $(window);
+      var el = $(element);
+      if (el.offset()==undefined) return;
+      var winPos = win.scrollTop() + win.height();
+      var elPos = el.offset().top + el.height();
+
+      if( winPos > elPos ) {
+          visibleCallback(el);
+      }  else {
+          obstructedCallback(el);
+      }     
+   }
+
+    $( window ).resize(function() {
+      fn();
+    })   
+
+   //This is a super hack, trying to find something that fires after
+   //the DOM has painted so that the elements have their position information.
+   //Right now, simply waiting 5 seconds and then manually checking the 
+   //position of the element.  
+   //TODO: Still need to find a way to initiate the check after the
+   //dom-if fires on the template
+   window.addEventListener('WebComponentsReady', function(e) {
+     setTimeout(fn,5000);
+   });   
+  }
 
   plugins.NewUI.prototype.listen = function listen(){
     // Fixes timing issues with existence of cockpit_int at time of initial call

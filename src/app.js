a43c0e74e78f13aa207a1ba49c2aa8cffbcf2f04
BrianAdams <brian@babastik.com>
2016-08-13 02:53:44 -0700

    cleanup

---
+++ src/app.js
@@ -0,0 +1,30 @@
+var forever = require('forever-monitor');
+var child = new forever.Monitor(__dirname + /cockpit.js', {
+    max: 3,
+    silent: process.env.NODE_DEBUG === 'false',
+    args: [],
+    'logFile': '/var/log/openrov.log',
+    'outFile': '/var/log/openrov.log',
+    'errFile': '/var/log/openrov.log'
+  });
+child.on('exit', function () {
+  console.log('cockpit.js has exited after 3 restarts');
+});
+child.on('exit:code', function(err){
+    console.log('detected error');
+    if (err==17){  //Graceful restarts of the app need to process.exit(17)
+        console.log('detected intentional restart');
+        child.times+=-1;
+    }
+});
+if (process.platform === 'linux') {
+  process.on('SIGTERM', function () {
+    console.error('got SIGTERM, shutting down...');
+    child.stop();
+  });
+  process.on('SIGINT', function () {
+    console.error('got SIGINT, shutting down...');
+    child.stop();
+  });
+}
+child.start();

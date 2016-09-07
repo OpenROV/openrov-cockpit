2d462e1220f5e22b8c602fe1777ba32c0e4050a9
BrianAdams <brian@babastik.com>
2016-09-07 10:11:49 -0700

    Fixed issue with app state in bower

--- install_lib/bower_install_all.js
+++ install_lib/bower_install_all.js
@@ -1,4 +1,5 @@
 var find = require('findit');
+var path = require('path');
 var finder = find(process.cwd());
 var bower = require('bower');
 var rimraf = require('rimraf');
@@ -28,6 +29,12 @@ finder.on('directory', function (dir, stat, stop) {
 finder.on('end', function () {
   installbower(0, bowersToInstall);
 });
+var packed_root = null;
+var packed_dir = null
+if (process.env.PACKDIR){
+  packed_root = path.dirname(process.env.PACKDIR);
+  packed_basename = path.basename(process.env.PACKDIR);
+}
 var installbower = function (index, array) {
   var dir = array[index];
   console.log('======== cleaning =======');
@@ -35,10 +42,11 @@ var installbower = function (index, array) {
   rimraf(dir + '/bower_components', function () {
     console.log('======== installing =======');
     console.log(dir);
-    bower.commands.install(['bower.json'], {
+    console.log(process.env.PACKDIR)
+    bower.commands.install([], {
       save: false,
       forceLatest: true
-    }, { cwd: dir }).on('error', function (err) {
+    }, { cwd: dir, directory:process.env.PACKDIR }).on('error', function (err) {
       console.error(err.message);
       process.exit(1);
     }).on('log', function (info) {
@@ -50,5 +58,6 @@ var installbower = function (index, array) {
         installbower(index, array);
       }
     });
+
   });
 };

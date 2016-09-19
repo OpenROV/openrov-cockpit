284693cf9cb87ebcbe0c03fd96151f97f2ca9005
Charles Cross <charles@openrov.com>
2016-09-19 11:57:50 -0700

    Added hashing capability to code generation step

--- src/lib/ArduinoBuilder.js
+++ src/lib/ArduinoBuilder.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs-extra'));
-var spawnAsync = require('child-process-promise')
-    .spawn;
+var spawnAsync = require('child-process-promise').spawn;
+var execAsync = require('child-process-promise').exec;
+
 
 var ArduinoBuilder = function() {};
 
@@ -12,6 +13,7 @@ ArduinoBuilder.prototype.BuildSketch = function(options, onStdout, onStderr) {
     var sketchName = '';
     var stagedSketchDir = '';
     var installDir = '';
+    var hash = '';
 
     return Promise.try(function()
         {
@@ -48,11 +50,15 @@ ArduinoBuilder.prototype.BuildSketch = function(options, onStdout, onStderr) {
         })
         .then(function()
         {
-            console.log('Removed old sketch');
+          console.log('Removed old sketch');
 
+          // Handle code generation, if requested
+          if( options.generateCode )
+          {
             // Generate preproc header file
             var output = '#pragma once\n\n';
 
+            // Create defines
             for (i = 0; i < options.preproc.length; ++i)
             {
                 output += '#define ' +
@@ -61,40 +67,68 @@ ArduinoBuilder.prototype.BuildSketch = function(options, onStdout, onStderr) {
                     .replace(',', ' ') + '\n';
             }
 
-            return fs.writeFileAsync(path.join(stagedSketchDir, 'CompileOptions.h'), output);
+            // Write CompileOptions.h file
+            return fs.writeFileAsync(path.join(stagedSketchDir, 'CompileOptions.h'), output)
+            .then( function()
+            {
+              console.log( 'Wrote CompileOptions.h' );
+
+              var pluginDirs = GetDirectories('/opt/openrov/cockpit/src/plugins');
+              var pluginString = '#pragma once\n\n';
+
+              // Find all plugin folders and generate a Plugins.h file that pulls them in
+              return Promise.map(pluginDirs, function(pluginName)
+              {
+                  var pluginDir = path.join('/opt/openrov/cockpit/src/plugins', pluginName);
+
+                  // Check for a firmware folder
+                  return fs.statAsync(path.join(pluginDir, 'firmware'))
+                      .then(function()
+                      {
+                          // Copy all files to the sketch directory
+                          return fs.copyAsync(path.join(pluginDir, 'firmware'), stagedSketchDir);
+                      })
+                      .then(function() 
+                      {
+                          // Add include for plugin to Plugins.h 
+                          pluginString += '#include "' + pluginName + '.h"\n';
+                      })
+                      .catch(function(err) 
+                      {
+                        // Ignore errors. Most plugins don't have firmware folders and failing to copy a plugin isn't fatal
+                      });
+              })
+              .then(function()
+              {
+                  // Write the generated include string to Plugins.h
+                  return fs.writeFileAsync(path.join(stagedSketchDir, 'Plugins.h'), pluginString);
+              });
+            })
+            .then( function()
+            {
+              // Generate a hash for everything in stagedSketchDir
+              // TODO: Make this cross platform and not a linux command call
+              return execAsync("find " + stagedSketchDir + " -name \\*.h -print0 -o -name \\*.hpp -print0 -o -name \\*.c -print0 -o -name \\*.cpp -print0 | xargs -0 sha1sum | sha1sum | awk '{print $1}'")
+              .then( function (result) 
+              {
+                var hash = result.stdout.trim();
+
+                // Should look something like: "ver:<<{{10024121ae3fa7fc60a5945be1e155520fb929dd}}>>;"
+                var hashDef = "#define VERSION_HASH F(\"ver:<<{{" + hash + "}}>>;\")\n";
+
+                // Append a define to the compile options
+                return fs.appendFileAsync( path.join(stagedSketchDir, 'CompileOptions.h'), hashDef );
+              })
+              
+              // TODO:
+              // Generate hash for the board core
+              // Generate hash for all libraries used
+              // Generate final hash for the combined generated hashes
+            });
+          }
         })
         .then(function()
         {
-            console.log('Made compileoptions');
-
-            var pluginDirs = GetDirectories('/opt/openrov/cockpit/src/plugins');
-            var pluginString = '#pragma once\n\n';
-
-            return Promise.map(pluginDirs, function(pluginName)
-                {
-                    var pluginDir = path.join('/opt/openrov/cockpit/src/plugins', pluginName);
-                    // Check for a firmware folder
-                    return fs.statAsync(path.join(pluginDir, 'firmware'))
-                        .then(function()
-                        {
-                            // Copy all files to the sketch directory
-                            return fs.copyAsync(path.join(pluginDir, 'firmware'), stagedSketchDir);
-                        })
-                        .then(function() {
-                            // Add include for plugin to Plugins.h 
-                            pluginString += '#include "' + pluginName + '.h"\n';
-                        })
-                        .catch(function(err) {});
-                })
-                .then(function()
-                {
-                    return fs.writeFileAsync(path.join(stagedSketchDir, 'Plugins.h'), pluginString);
-                });
-        })
-        .then(function()
-        {
-            console.log('Made plugins.h');
-
             buildOpts.push('-fqbn', options.fqbn);
             buildOpts.push(stagedSketchDir);
 
@@ -133,32 +167,40 @@ ArduinoBuilder.prototype.BuildSketch = function(options, onStdout, onStderr) {
                     })
                 ]);
         })
-        .then(function(firmwareFile) {
+        .then(function(firmwareFile) 
+        {
             console.log('SUCCESS');
 
-            return Promise.try(function() {
-                    if (options.cleanAfterBuild) {
+            return Promise.try(function() 
+            {
+                    if (options.cleanAfterBuild) 
+                    {
                         // Clean up temp dir
                         return fs.removeAsync(options.buildDir)
-                            .then(function() {
+                            .then(function() 
+                            {
                                 return fs.removeAsync('/opt/openrov/firmware/staged');
                             });
                     }
                 })
-                .catch(function(error) {
+                .catch(function(error) 
+                {
                     console.log("Strange error while trying to cleanup firmware staged files: " + error.message);
                 })
-                .then(function() {
+                .then(function() 
+                {
                     return firmwareFile;
                 });
 
         })
-        .catch(function(err) {
+        .catch(function(err) 
+        {
             if (options.cleanAfterBuild)
             {
                 // Clean up temp dir first
                 return fs.removeAsync(options.buildDir)
-                    .then(function() {
+                    .then(function() 
+                    {
                         // Rethrow error
                         throw err;
                     });
@@ -170,10 +212,13 @@ ArduinoBuilder.prototype.BuildSketch = function(options, onStdout, onStderr) {
         });
 };
 
-function ProcessOptions(options) {
+function ProcessOptions(options) 
+{
     // First, validate required options
     ValidateOptions(options);
-    var defaults = {
+
+    var defaults = 
+    {
         sketchDir: '',
         productID: '',
         buildDir: '/opt/openrov/firmware/build',
@@ -187,46 +232,67 @@ function ProcessOptions(options) {
         quiet: false,
         debug: 5,
         libs: [],
-        preproc: []
+        preproc: [],
+        generateCode: true
     };
+
     // Override defaults with options
     options = options || {};
-    for (var opt in defaults) {
-        if (defaults.hasOwnProperty(opt) && !options.hasOwnProperty(opt)) {
+
+    for (var opt in defaults) 
+    {
+        if (defaults.hasOwnProperty(opt) && !options.hasOwnProperty(opt))
+        {
             options[opt] = defaults[opt];
         }
     }
     // Create optArray
     var optArray = [];
-    if (options.verbose) {
+
+    if (options.verbose) 
+    {
         optArray.push('-verbose');
     }
-    if (options.quiet) {
+
+    if (options.quiet) 
+    {
         optArray.push('-quiet');
     }
+
     optArray.push('-compile');
     optArray.push('-warnings', options.warnings);
     optArray.push('-build-path', options.buildDir);
     optArray.push('-hardware', options.hardware);
     optArray.push('-tools', options.tools);
-    for (i = 0; i < options.libs.length; ++i) {
+
+    for (i = 0; i < options.libs.length; ++i) 
+    {
         optArray.push('-libraries', options.libs[i]);
     }
+
     return optArray;
 }
 
-function ValidateOptions(options) {
+function ValidateOptions(options) 
+{
     // Required parameters
-    if (IsBlank(options.sketchDir)) {
+    if (IsBlank(options.sketchDir)) 
+    {
         throw new Error('Missing required option: sketchDir');
     }
-    if (IsBlank(options.productID)) {
+
+    if (IsBlank(options.productID)) 
+    {
         throw new Error('Missing required option: productID');
     }
-    if (IsBlank(options.installBaseDir)) {
+
+    if (IsBlank(options.installBaseDir)) 
+    {
         throw new Error('Missing required option: installBaseDir');
     }
-    if (IsBlank(options.fqbn)) {
+
+    if (IsBlank(options.fqbn)) 
+    {
         throw new Error('Missing required option: fqbn');
     }
 }
@@ -235,11 +301,19 @@ function IsBlank(str) {
     return !str || /^\s*$/.test(str);
 }
 
-function GetDirectories(dir) {
+function GetDirectories(dir) 
+{
     return fs.readdirSync(dir)
-        .filter(function(file) {
+        .filter(function(file) 
+        {
             return fs.statSync(path.join(dir, file))
                 .isDirectory();
         });
 }
+
+function HashDirectory( pathIn )
+{
+  
+}
+
 module.exports = new ArduinoBuilder();

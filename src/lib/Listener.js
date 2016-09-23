1532918078ec4aba35f9d54c9f12f87d83e7ae21
Charles Cross <charles@openrov.com>
2016-09-23 15:09:16 -0700

    Created Listener and Periodic helper libraries

---
+++ src/lib/Listener.js
@@ -0,0 +1,50 @@
+class Listener
+{
+    constructor( bus, event, withHistory, func )
+    {
+        // Private members
+        this.bus = bus;
+        this.event = event;
+        this.withHistory = withHistory;
+        this.func = func;
+
+        this.enabled = false;
+    }
+
+    enable()
+    {
+        if( this.enabled === false )
+        {
+            // Handle registration with history. If unsupported, fall back to normal registration
+            if( this.withHistory )
+            {
+                if( this.bus.withHistory !== undefined )
+                {
+                    this.bus.withHistory.on( this.event, this.func );
+                }
+                else
+                {
+                    console.warn( "EventEmitter doesn't support withHistory.on(). Using default on(): When registering event: " + this.event );
+                    this.bus.on( this.event, this.func );
+                }
+            }
+            else
+            {
+                this.bus.on( this.event, this.func );
+            }
+
+            this.enabled = true;
+        }
+    }
+
+    disable()
+    {
+        if( this.enabled === true )
+        {
+            this.bus.off( event, func );
+            this.enabled = false;
+        }
+    }
+}
+
+module.exports = Listener;

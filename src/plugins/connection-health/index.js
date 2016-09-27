f8108abfe668f0fb4f18d6f35b0fddd043a528ea
BrianAdams <brian@babastik.com>
2016-09-27 13:23:43 -0700

    Connection pings fixed

--- src/plugins/connection-health/index.js
+++ src/plugins/connection-health/index.js
@@ -3,8 +3,11 @@ function ConnectionHealth(name, deps) {
   var lastping = 0;
   this.deps = deps;
   var self = this;
-  deps.cockpit.on('ping', function (id) {
-    self.deps.cockpit.emit('pong', id);
+  deps.cockpit.onAny(function(e){
+    console.log(this.event);
+  });
+  deps.cockpit.on('sys.ping', function (id) {
+    self.deps.cockpit.emit('sys.pong', id);
     if (new Date().getTime() - lastping > 500) {
       self.deps.globalEventLoop.emit('mcu.SendCommand', 'ping(0)');
       lastping = new Date().getTime();

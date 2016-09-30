d3213e0b85b56b37a2a03948704f236bff01d55f
Charles Cross <charles@openrov.com>
2016-09-29 23:57:35 -0700

    Fixed paths in esc flash script

--- src/system-plugins/platform-manager/platforms/beaglebone/boards/2x/install/files/FlashESCS.sh
+++ src/system-plugins/platform-manager/platforms/beaglebone/boards/2x/install/files/FlashESCS.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "/opt/openrov/firmware/bin/2x/afro" ]; then
+if [ ! -d "/opt/openrov/firmware/bin/afro" ]; then
   echo "Cannot find the afro_esc firmware project. Make sure it has been installed."
   exit 1
 fi
 
-avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:/opt/openrov/firmware/bin/2x/afro/afro_nfet.hex:i -vv
+avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:/opt/openrov/firmware/bin/afro/afro_nfet.hex:i -vv
 stty -F /dev/ttyO1 raw 19200
 echo -n "\$M<P52" > /dev/ttyO1
-avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:/opt/openrov/firmware/bin/2x/afro/afro_nfet.hex:i -vv
+avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:/opt/openrov/firmware/bin/afro/afro_nfet.hex:i -vv
 echo -n "\$M<P53" > /dev/ttyO1
-avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:/opt/openrov/firmware/bin/2x/afro/afro_nfet.hex:i -vv
+avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:/opt/openrov/firmware/bin/afro/afro_nfet.hex:i -vv

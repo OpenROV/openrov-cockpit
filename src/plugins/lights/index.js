051eea162e7ade4abafc9103d3beb15e8993cde4
Charles Cross <charles@openrov.com>
2016-09-30 18:20:29 -0700

    Reworked lights node plugin

--- src/plugins/lights/index.js
+++ src/plugins/lights/index.js
@@ -1,85 +1,175 @@
-(function () {
-  function Lights(name, deps) {
-    console.log('Lights plugin loaded');
-    var self = this;
-    self.setting = 0;
-    //  Settings:       = [ 0 .. 5 ]
-    self.levelMap = [
-      0,
-      80,
-      100,
-      130,
-      190,
-      255
-    ];
-    self.maxLevel = self.levelMap.length - 1;
-    // Cockpit
-    deps.cockpit.on('plugin.lights.toggle', function () {
-      toggleLights();
-    });
-    deps.cockpit.on('plugin.lights.adjust', function (value) {
-      adjustLights(value);
-    });
-    deps.cockpit.on('plugin.lights.set', function (value) {
-      setLights(value);
-    });
-    deps.cockpit.on('plugin.lights.setOnOff', function (setOn) {
-      if (setOn) {
-        // Max light power
-        setLights(self.maxLevel);
-      } else {
-        // Min light power
-        setLights(0);
-      }
-    });
-    // Arduino
-    deps.globalEventLoop.on('mcu.status', function (data) {
-      if ('LIGT' in data) {
-        // Value of 0-255 representing percent
-        var level = parseInt(data.LIGT);
-        // Search for the level in the level map
-        var setting = self.levelMap.indexOf(level);
-        if (setting != -1) {
-          // The new setting value is the array index of the level in the level map, if it exists
-          self.setting = setting;
-        } else {
-          // Find the closest level in our map
-          var closest = self.levelMap.reduce(function (prev, curr) {
-              return Math.abs(curr - level) < Math.abs(prev - level) ? curr : prev;
+(function() 
+{
+    const Periodic = require( 'Periodic' );
+    const Listener = require( 'Listener' );
+
+    // Encoding helper functions
+    function encode( floatIn )
+    {
+        return parseInt( floatIn * 1000 );
+    }
+
+    function decode( intIn )
+    {
+        return ( intIn * 0.001 );
+    }
+
+    class Lights
+    {
+        constructor(name, deps)
+        {
+            console.log( 'Lights plugin loaded' );
+
+            this.globalBus  = deps.globalEventLoop;
+            this.cockpitBus = deps.cockpit;
+
+            this.targetPower          = 0;
+            this.targetPower_enc      = 0;
+            this.mcuTargetPower_enc   = 0;
+
+            var self = this;
+
+            this.SyncTargetPower = new Periodic( 33, "timeout", function()
+            {
+                var synced = true;
+
+                // Send target power to MCU until it responds with affirmation
+                if( self.mcuTargetPower_enc !== self.targetPower_enc )
+                {
+                    synced = false;
+
+                    // Encode floating point to integer representation
+                    var command = 'lights_tpow(' + self.targetPower_enc + ')';
+
+                    // Emit command to mcu
+                    self.globalBus.emit( 'mcu.SendCommand', command );
+                }
+
+                if( synced )
+                {
+                    // No need to continue
+                    self.SyncTargetPower.stop();
+                }
             });
-          // Set the new setting value based on the index of the closest level
-          self.setting = self.levelMap.indexOf(closest);
+
+            this.listeners = 
+            {
+                settings: new Listener( this.globalBus, 'settings-change.lights', true, function( settings )
+                {
+                    // Apply settings
+                    self.settings = settings.lights;
+                    
+                    // Emit settings update to cockpit
+                    self.cockpitBus.emit( 'plugin.lights.settingsChange', self.settings );
+
+                    // Enable MCU Status listener
+                    self.listeners.mcuStatus.enable();
+
+                    // Enable API
+                    self.listeners.setTargetPower.disable();
+                }),
+
+                mcuStatus: new Listener( this.globalBus, 'mcu.status', false, function( data )
+                {
+                    // Current light power
+                    if( 'lights_pow' in data ) 
+                    {
+                        // Convert from integer to float
+                        var power = decode( parseInt( data.lights_pow ) );
+
+                        // Emit on cockpit bus for UI purposes
+                        self.cockpitBus.emit( 'plugin.lights.currentPower', power );
+                    }
+
+                    // Target light power
+                    if( 'lights_tpow' in data ) 
+                    {
+                        // Save encoded version for sync validation purposes
+                        self.mcuTargetPower_enc = parseInt( data.lights_tpow );
+
+                        // Convert from integer to float
+                        var power = decode( self.mcuTargetPower_enc );
+
+                        // Emit the real target power on the cockpit bus for UI purposes
+                        self.cockpitBus.emit( 'plugin.lights.targetPower', power );
+                    }
+                }),
+
+                setTargetPower: new Listener( this.cockpitBus, 'plugin.lights.setTargetPower', false, function( powerIn )
+                {
+                    // Set new target Power
+                    self.setTargetPos( powerIn );
+                })
+            }
         }
-        deps.cockpit.emit('plugin.lights.state', { level: self.setting });
-      }
-    });
-    var adjustLights = function adjustLights(value) {
-      // Modify current setting
-      setLights(self.setting + value);
+
+        setTargetPower( powerIn )
+        {
+            var self = this;
+
+            // Validate input
+            if( isNaN( powerIn ) )
+            {
+              // Ignore
+            }
+
+            // Apply limits
+            if( powerIn > 1.0 )
+            {
+                self.targetPower = 1.0;
+            }
+            else if( powerIn < 0 )
+            {
+                self.targetPower = 0;
+            }
+            else
+            {
+                self.targetPower = powerIn;
+            }
+
+            self.targetPower_enc = encode( self.targetPower );
+
+            // Start targetPower sync, if not already running
+            self.SyncTargetPower.start();
+        }
+        
+        start()
+        {
+          this.listeners.settings.enable();
+        }
+
+        stop()
+        {
+          this.listeners.settings.disable();
+          this.listeners.mcuStatus.disable();
+          this.listeners.setTargetPower.disable();
+        }
+
+        getSettingSchema()
+        {
+            //from http://json-schema.org/examples.html
+            return [{
+                'title': 'Lights',
+                'type': 'object',
+                'id': 'lights',
+                'properties': {
+                    'steps': 
+                    {
+                        'type': 'number',
+                        'default': 5
+                    }     
+                },
+
+                // TODO: Need to classify which settings are used at various levels: MCU Model, Local Model, UI model
+                'required': [
+                    'steps'        // (UI)
+                ]
+            }];
+        }
+    }
+
+    module.exports = function(name, deps) 
+    {
+        return new Lights(name, deps);
     };
-    var toggleLights = function toggleLights() {
-      if (self.setting > 0) {
-        // Set to min power
-        setLights(0);
-      } else {
-        // Set to max power
-        setLights(self.maxLevel);
-      }
-    };
-    var setLights = function setLights(value) {
-      // Range limit the new setting from 0 to the max number of defined levels
-      if (value < 0) {
-        value = 0;
-      } else if (value >= self.maxLevel) {
-        value = self.maxLevel;
-      }
-      // Make sure the new setting is an integer
-      self.setting = Math.round(value);
-      var command = 'ligt(' + self.levelMap[self.setting] + ')';
-      deps.globalEventLoop.emit('mcu.SendCommand', command);
-    };
-  }
-  module.exports = function (name, deps) {
-    return new Lights(name, deps);
-  };
 }());

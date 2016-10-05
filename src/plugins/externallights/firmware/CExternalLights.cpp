6be7b9c424536efd0325474f4b3d997d568e1833
Charles Cross <charles@openrov.com>
2016-10-04 21:45:15 -0700

    Updated External Lights firmware

--- src/plugins/externallights/firmware/CExternalLights.cpp
+++ src/plugins/externallights/firmware/CExternalLights.cpp
@@ -4,73 +4,154 @@
 // Includes
 #include <Arduino.h>
 #include "CExternalLights.h"
-#include "CPin.h"
-
-// Set pin definitions
-#define ELIGHTS0_PIN 46
-#define ELIGHTS1_PIN 12
+#include "NCommManager.h"
+#include "NVehicleManager.h"
 
 namespace
 {
-    CPin elight0("elight0", ELIGHTS0_PIN, CPin::kAnalog, CPin::kOutput);
-    CPin elight1("elight1", ELIGHTS1_PIN, CPin::kAnalog, CPin::kOutput);
+	// 1% per 10ms
+	const float kPowerDelta = 0.01f;
+
+	inline uint32_t PercentToAnalog( float x )
+	{	
+		if( x < 0.0f )
+		{
+			return 0;
+		}
+		else if( x < 0.33f )
+		{
+			// Linear region, 0-80
+			return static_cast<uint32_t>( 242.424f * x );
+		}
+		else
+		{
+			// Parabolic region 80-255
+			return static_cast<uint32_t>( ( 308.571f * x * x ) - ( 147.426f * x ) + 95.0f );
+		}
+	}
+}
+
+CExternalLights::CExternalLights( uint32_t pinIn )
+	: m_pin( "elight", pinIn, CPin::kAnalog, CPin::kOutput )
+{
 }
 
 void CExternalLights::Initialize()
 {
-    elight0.Reset();
-    elight0.Write(0);
-    
-    elight1.Reset();
-    elight1.Write(0);
+	NVehicleManager::m_capabilityBitmask |= ( 1 << LIGHTS_CAPABLE );
+
+	// Reset pin
+	m_pin.Reset();
+	m_pin.Write( 0 );
+
+	// Reset timers
+    m_controlTimer.Reset();
+    m_telemetryTimer.Reset();
 }
 
 void CExternalLights::Update( CCommand& commandIn )
 {
-    if( commandIn.Equals( "elight0" ) )
+	// Check for messages
+	if( !NCommManager::m_isCommandAvailable )
 	{
-		// Should be between 0-255, with 255 being full brightness
-		int value = commandIn.m_arguments[1];
+		return;
+	}
 
-		// Bounds corrections
-		if( value < 0 )
+	// Handle messages
+	if( commandIn.Equals( "elights_tpow" ) )
+	{
+		// Update the target position
+		m_targetPower = util::Decode1K( commandIn.m_arguments[1] );
+
+		// TODO: Ideally this unit would have the ability to autonomously set its own target and ack receipt with a separate mechanism
+		// Acknowledge target position
+		Serial.print( F( "elights_tpow:" ) );
+		Serial.print( commandIn.m_arguments[1] );
+		Serial.println( ';' );
+
+		// Pass through linearization function
+		m_targetPower_an = PercentToAnalog( m_targetPower );
+
+		// Apply ceiling
+		if( m_targetPower_an > 255 )
 		{
-			value = 0;
+			m_targetPower_an = 255;
 		}
-		if( value > 255 )
-		{
-			value = 255;
-		}
-		
-		elight0.Write( value );
-		
-		Serial.print( F( "LIGTE0:" ) );
-		Serial.print( value );
+
+		// Directly move to target power
+		m_currentPower 		= m_targetPower;
+		m_currentPower_an 	= m_targetPower_an;
+
+		// Write the power value to the pin
+		m_pin.Write( m_currentPower_an );
+
+		// Emit current power
+		Serial.print( F( "elights_pow:" ) );
+		Serial.print( util::Encode1K( m_currentPower ) );
 		Serial.print( ';' );
 	}
-    
-    // Handle messages
-	if( commandIn.Equals( "elight1" ) )
-	{
-		// Should be between 0-255, with 255 being full brightness
-		int value = commandIn.m_arguments[1];
 
-		// Bounds corrections
-		if( value < 0 )
-		{
-			value = 0;
-		}
-		if( value > 255 )
-		{
-			value = 255;
-		}
-		
-		elight1.Write( value );
-		
-		Serial.print( F( "LIGTE1:" ) );
-		Serial.print( value );
-		Serial.print( ';' );
-	}
+	// TODO: Disabled until discrepancies resolved.
+	// Run servo adjustment at 100Hz
+    // if( m_controlTimer.HasElapsed( 10 ) )
+    // {
+	// 	if( m_currentPower_an != m_targetPower_an )
+	// 	{
+	// 		float error = m_targetPower - m_currentPower;
+
+	// 		// Manage transition speed (100% per second)
+	// 		if( abs( error ) < kPowerDelta )
+	// 		{
+	// 			// Move directly to targets. No need to calculate conversions
+	// 			m_currentPower 		= m_targetPower;
+	// 			m_currentPower_an 	= m_targetPower_an;
+	// 		}
+	// 		else
+	// 		{
+	// 			// Move currentPower by kPowerDelta increment
+	// 			m_currentPower += ( ( error < 0.0f ) ? -kPowerDelta : kPowerDelta );
+
+	// 			// Update the analog power representation
+	// 			m_currentPower_an = PercentToAnalog( m_currentPower );
+
+	// 			// Apply ceiling
+	// 			if( m_currentPower_an > 255 )
+	// 			{
+	// 				m_currentPower_an = 255;
+	// 			}
+	// 		}
+
+	// 		// Write the power value to the pin
+	// 		m_pin.Write( m_currentPower_an );
+
+	// 		// Snap floating point power to target power to handle any potential float<->int errors
+	// 		if( m_currentPower_an == m_targetPower_an )
+	// 		{
+	// 			m_currentPower_an = m_targetPower_an;
+	// 		}
+	// 	}
+
+	// 	Serial.print( F( "lights_pow:" ) );
+	// 	Serial.print( util::Encode1K( m_currentPower ) );
+	// 	Serial.print( ';' );
+
+	// 	Serial.print( F( "test_light:" ) );
+	// 	Serial.print( m_currentPower_an );
+	// 	Serial.print( ';' );
+	// }
+
+	// // Emit power telemetry at 10Hz, but only on changes
+    // if( m_telemetryTimer.HasElapsed( 100 ) )
+    // {
+	// 	if( m_lastPower_an != m_currentPower_an )
+	// 	{
+	// 		Serial.print( F( "lights_pow:" ) );
+	// 		Serial.print( util::Encode1K( m_currentPower ) );
+	// 		Serial.print( ';' );
+
+	// 		m_lastPower_an = m_currentPower_an;
+	// 	}
+    // }
 }
 
 #endif
--- src/plugins/externallights/firmware/CExternalLights.h
+++ src/plugins/externallights/firmware/CExternalLights.h
@@ -2,10 +2,26 @@
 
 // Includes
 #include "CModule.h"
+#include "CPin.h"
+#include "CTimer.h"
 
 class CExternalLights : public CModule
 {
 public:
+    CExternalLights( uint32_t pinIn );
     void Initialize();
     void Update( CCommand& commandIn );
+
+private:
+    CTimer      m_controlTimer;
+    CTimer      m_telemetryTimer;
+    CPin        m_pin;
+
+    float       m_targetPower = 0.0f;
+    float       m_currentPower = 0.0f;
+
+    uint32_t    m_lastPower_an = 0;
+    uint32_t    m_targetPower_an = 0;
+    uint32_t    m_currentPower_an = 0;
 };
+

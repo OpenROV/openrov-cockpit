9795d55ee5ed787815ef64faf92d4d1a11df98b8
Charles Cross <charles@openrov.com>
2016-10-05 18:33:02 -0700

    Updated instantiation of example plugin

--- src/plugins/example/firmware/example.h
+++ src/plugins/example/firmware/example.h
@@ -1,14 +1,7 @@
 #pragma once
 
 // This is the object instantiation file. Eventually, this will be automatically generated in cockpit.
-// Until then, you need to check the SysConfig file to see if the module has been enabled.
-// If it has been enabled, you can create an instance of the module!
-// You can also just skip the SysConfig check, but no guarantees you won't break anything!
+// Create an instance of your plugin!
 
-#include "SysConfig.h"
-#if(HAS_EXAMPLE_PLUGIN)
-
-	#include "CExample.h"
-	CExample example;
-	
-#endif
+#include "CExample.h"
+CExample example;

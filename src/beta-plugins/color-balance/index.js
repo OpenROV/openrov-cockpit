ef826d1ef7e39636b407fd16f7ee55801d886d74
Gilbert Montague <gilbert.i.montague@gmail.com>
2016-10-10 18:51:11 -0700

    removed preemptive color balance push

--- src/beta-plugins/color-balance/index.js
+++
@@ -1,7 +0,0 @@
-function colorBalance(name, deps) {
-    console.log("Node process for Color Balance plugin.");
-}
-
-module.exports = function(name, deps) {
-    return new colorBalance(name,deps);
-};

693f12ff5dde60549aeb6cd93a25f26c1d192dd9
Charles Cross <charles@openrov.com>
2016-10-21 19:03:47 -0700

    Removed firmware plugins

--- src/plugins/example/firmware/example.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// This is the object instantiation file. Eventually, this will be automatically generated in cockpit.
-// Create an instance of your plugin!
-
-#include "CExample.h"
-CExample example;

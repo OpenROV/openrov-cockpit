ce148da7c912e40de86c2e8895d5d7445b990cc4
Charles Cross <charles@openrov.com>
2016-12-09 22:11:45 -0800

    Reverted Serialbridge back to the way it was for the 2x, created new bridge specifically for trident

--- src/system-plugins/platform-manager/modules/SerialBridge.js
+++ src/system-plugins/platform-manager/modules/SerialBridge.js
@@ -1,135 +1,161 @@
-const crc           = require('crc');
-const SerialPort    = require('serialport');
-const EventEmitter  = require('events').EventEmitter;
-
-class SerialBridge extends EventEmitter
-{
-  constructor( uartDevicePath, uartBaud )
-  {
-    super();
-
-    this.emitRawSerial = false;
-    this.serialConnected = false;
-    this.serialPort = {};
-  }
-
-  connect()
-  {
-    this.serialPort = new SerialPort( uartDevicePath, 
-    {
+var crc = require('crc');
+var SerialPort = require('serialport');
+var EventEmitter = require('events').EventEmitter;
+function Bridge(uartPath,uartBaud) {
+  var self = this;
+  var bridge = new EventEmitter();
+  var reader = new StatusReader();
+  var emitRawSerial = false;
+  var serialConnected = false;
+  var serialPort = {};
+  var lastWriteTime = new Date();
+  
+ bridge.isConnected = function(){
+   return serialConnected;
+ }
+  
+ bridge.connect = function () {
+    serialPort = new SerialPort(uartPath, {
       baudRate: uartBaud,
       autoOpen: true
     });
+    
+    var Readline 
+    var parser 
 
-    // All discrete firmware messages end with ';'
-    var parser = this.serialPort.pipe( new Delimiter( { delimiter: ';' } ) );
+    //Work around for influx serialport changes while we have the dependencies moving around a bit.
+    //Remove once the shrinkwrapped version matches the dev version api
+    if (SerialPort.parsers.Readline){
+      Readline= SerialPort.parsers.Readline; 
+      parser=serialPort.pipe(Readline({delimiter: '\r\n'}));   
+    }
+    if (SerialPort.parsers.ReadLine){
+      Readline= SerialPort.parsers.ReadLine; 
+      parser=serialPort.pipe(Readline({delimiter: '\r\n'}));   
+    }
+    if (SerialPort.parsers.readline){
+      Readline= SerialPort.parsers.readline; 
+      parser=serialPort.pipe(Readline({delimiter: '\r\n'}));   
+    }    
 
-    this.serialPort.on('open', () =>
-    {
-      this.serialConnected = true;
+
+    serialPort.on('open', function () {
+      serialConnected = true;
       console.log('Serial port opened!');
     });
-
-    this.serialPort.on('error',(err) =>
-    {
+    serialPort.on('error',function(err){
       console.log('Serial error',err)
     })
-
-    this.serialPort.on('close', (data) =>
-    {
+    serialPort.on('close', function (data) {
       console.log('Serial port closed!');
-      this.serialConnected = false;
+      serialConnected = false;
     });
+    parser.on('data', function (data) {
+      var status = reader.parseStatus(data);
+      bridge.emit('status', status);
+      if (emitRawSerial) {
+        bridge.emit('serial-recieved', data + '\n');
+      }
+    });
+  };
 
-    parser.on('data', (data) =>
-    {
-      var status = this.parseStatus(data);
-
-      // If valid status message received, emit status events
-      if( status !== null )
-      {
-        this.emit('status', status);
-
-        if( this.emitRawSerial ) 
-        {
-          this.emit('serial-recieved', data + '\n');
+  // This code intentionally spaces out the serial commands so that the buffer does not overflow
+  bridge.write = function (command) {
+    var crc8 = crc.crc81wire(command);
+    var commandBuffer = new Buffer(command, 'utf8');
+    var crcBuffer = new Buffer(1);
+    crcBuffer[0] = crc8;
+    var messagebuffer = Buffer.concat([
+        crcBuffer,
+        commandBuffer
+      ]);
+    if (serialConnected) {
+      var now = new Date();
+      var delay = 3 - (now.getTime() - lastWriteTime.getTime());
+      if (delay < 0) {
+        delay = 0;
+      }
+      lastWriteTime = now;
+      lastWriteTime.setMilliseconds(lastWriteTime.getMilliseconds + delay);
+      setTimeout(function () {
+        serialPort.write(messagebuffer);
+        if (emitRawSerial) {
+          bridge.emit('serial-sent', command);
         }
-      }
-    });
-  }
-
-  close()
-  {
-    if( !this.serialConnected )
-    {
-      return;
-    }
-
-    this.serialConnected = false;
-
-    //This code is a work around for a race condition in the serial port code https://github.com/voodootikigod/node-serialport/issues/241#issuecomment-43058353
-    var sp = this.serialPort;
-
-    this.serialPort.flush( (err) =>
-    {
-        this.serialPort.close( (err) => {} );
-    });
-  }
-
-  write( command )
-  {
-    // Create buffer for crc+command
-    let messagebuffer = new Buffer( command.length + 1 );
-
-    // Calculate and write crc8
-    messagebuffer[ 0 ] = crc.crc81wire(command);
-
-    // Write command
-    messagebuffer.write( command, 1, command.length, 'utf-8' );
-
-    if( this.serialConnected ) 
-    {
-      this.serialPort.write( messagebuffer );
-
-      if( this.emitRawSerial ) 
-      {
-        this.emit('serial-sent', command );
-      }
-    } 
-    else
-    {
+      }, delay);
+    } else {
       console.log('DID NOT SEND');
     }
-  }
-
-  parseStatus( rawStatus )
-  {
-    let parts   = rawStatus.split(';').split( ':' );
-    
-    if( parts.length === 2 )
-    {
-      let status = {};
-      status[ parts[ 0 ] ] = parts[ 1 ];
-      return status;
+  };
+  bridge.startRawSerialData = function startRawSerialData() {
+    emitRawSerial = true;
+  };
+  bridge.stopRawSerialData = function stopRawSerialData() {
+    emitRawSerial = false;
+  };
+  bridge.close = function () {
+    if (!serialConnected){
+      return;
     }
-
-    return null;
-  }
-
-  isConnected()
-  {
-    return this.serialConnected;
-  }
-
-  startRawSerialData()
-  {
-    this.emitRawSerial = true;
-  }
-
-  stopRawSerialData()
-  {
-    this.emitRawSerial = false;
-  }
+    serialConnected = false;
+    //This code is a work around for a race condition in the serial port code https://github.com/voodootikigod/node-serialport/issues/241#issuecomment-43058353
+    var sp = serialPort;
+    serialPort.flush(function (err) {
+    //  setTimeout(function () {
+        sp.close(function (err) {
+        });
+    //  }, 10);
+    });
+  };
+  // Listen for firmware settings updates
+  // TODO: Has this been deprecated for TSET?
+  reader.on('firmwareSettingsReported', function (settings) {
+    bridge.emit('firmwareSettingsReported', settings);
+  });
+  return bridge;
 }
-
-module.exports = SerialBridge;
+// Helper class for parsing status messages
+var StatusReader = function () {
+  var reader = new EventEmitter();
+  var currTemp = 20;
+  var currDepth = 0;
+  var processSettings = function processSettings(parts) {
+    var setparts = parts.split(',');
+    var settingsCollection = {};
+    for (var s = 0; s < setparts.length; s++) {
+      var lastParts = setparts[s].split('|');
+      settingsCollection[lastParts[0]] = lastParts[1];
+    }
+    reader.emit('firmwareSettingsReported', settingsCollection);
+    return settingsCollection;
+  };
+  var processItemsInStatus = function processItemsInStatus(status) {
+    if ('iout' in status) {
+      status.iout = parseFloat(status.iout);
+    }
+    if ('btti' in status) {
+      status.btti = parseFloat(status.btti);
+    }
+    if ('vout' in status) {
+      status.vout = parseFloat(status.vout);
+    }
+  };
+  reader.parseStatus = function parseStatus(rawStatus) {
+    var parts = rawStatus.split(';');
+    var status = {};
+    for (var i = 0; i < parts.length; i++) {
+      var subParts = parts[i].split(':');
+      switch (subParts[0]) {
+      case '*settings':
+        status.settings = processSettings(subParts[1]);
+        break;
+      default:
+        status[subParts[0]] = subParts[1];
+      }
+    }
+    processItemsInStatus(status);
+    return status;
+  };
+  return reader;
+};
+module.exports = Bridge;
---
+++ src/system-plugins/platform-manager/modules/TridentSerialBridge.js
@@ -0,0 +1,135 @@
+const crc           = require('crc');
+const SerialPort    = require('serialport');
+const EventEmitter  = require('events').EventEmitter;
+
+class SerialBridge extends EventEmitter
+{
+  constructor( uartDevicePath, uartBaud )
+  {
+    super();
+
+    this.emitRawSerial = false;
+    this.serialConnected = false;
+    this.serialPort = {};
+  }
+
+  connect()
+  {
+    this.serialPort = new SerialPort( uartDevicePath, 
+    {
+      baudRate: uartBaud,
+      autoOpen: true
+    });
+
+    // All discrete firmware messages end with ';'
+    var parser = this.serialPort.pipe( new Delimiter( { delimiter: ';' } ) );
+
+    this.serialPort.on('open', () =>
+    {
+      this.serialConnected = true;
+      console.log('Serial port opened!');
+    });
+
+    this.serialPort.on('error',(err) =>
+    {
+      console.log('Serial error',err)
+    })
+
+    this.serialPort.on('close', (data) =>
+    {
+      console.log('Serial port closed!');
+      this.serialConnected = false;
+    });
+
+    parser.on('data', (data) =>
+    {
+      var status = this.parseStatus(data);
+
+      // If valid status message received, emit status events
+      if( status !== null )
+      {
+        this.emit('status', status);
+
+        if( this.emitRawSerial ) 
+        {
+          this.emit('serial-recieved', data + '\n');
+        }
+      }
+    });
+  }
+
+  close()
+  {
+    if( !this.serialConnected )
+    {
+      return;
+    }
+
+    this.serialConnected = false;
+
+    //This code is a work around for a race condition in the serial port code https://github.com/voodootikigod/node-serialport/issues/241#issuecomment-43058353
+    var sp = this.serialPort;
+
+    this.serialPort.flush( (err) =>
+    {
+        this.serialPort.close( (err) => {} );
+    });
+  }
+
+  write( command )
+  {
+    // Create buffer for crc+command
+    let messagebuffer = new Buffer( command.length + 1 );
+
+    // Calculate and write crc8
+    messagebuffer[ 0 ] = crc.crc81wire(command);
+
+    // Write command
+    messagebuffer.write( command, 1, command.length, 'utf-8' );
+
+    if( this.serialConnected ) 
+    {
+      this.serialPort.write( messagebuffer );
+
+      if( this.emitRawSerial ) 
+      {
+        this.emit('serial-sent', command );
+      }
+    } 
+    else
+    {
+      console.log('DID NOT SEND');
+    }
+  }
+
+  parseStatus( rawStatus )
+  {
+    let parts   = rawStatus.split(';').split( ':' );
+    
+    if( parts.length === 2 )
+    {
+      let status = {};
+      status[ parts[ 0 ] ] = parts[ 1 ];
+      return status;
+    }
+
+    return null;
+  }
+
+  isConnected()
+  {
+    return this.serialConnected;
+  }
+
+  startRawSerialData()
+  {
+    this.emitRawSerial = true;
+  }
+
+  stopRawSerialData()
+  {
+    this.emitRawSerial = false;
+  }
+}
+
+module.exports = SerialBridge;

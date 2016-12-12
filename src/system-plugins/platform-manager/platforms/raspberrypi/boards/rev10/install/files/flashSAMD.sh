a0224a06eae7ad839861127bb676dd120f299d64
Charles Cross <charles@openrov.com>
2016-12-12 15:59:08 -0800

    Fixed flash path

--- src/system-plugins/platform-manager/platforms/raspberrypi/boards/rev10/install/files/flashSAMD.sh
+++ src/system-plugins/platform-manager/platforms/raspberrypi/boards/rev10/install/files/flashSAMD.sh
@@ -5,4 +5,4 @@ set -ex
 ./selectSAMD.sh
 
 # Flash firmware
-openocd -f /usr/share/openocd/scripts/board/openrov_trident_samd.cfg -c "program /opt/openrov/firmware/deploy/samd/samd.bin; reset; exit"
+openocd -f /usr/share/openocd/scripts/board/openrov_trident_samd.cfg -c "program /opt/openrov/firmware/deploy/samd.bin; reset; exit"

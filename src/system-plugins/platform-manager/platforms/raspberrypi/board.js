004655142b872e75f800811828f304f95b6a1924
Charles Cross <charles@openrov.com>
2016-12-12 15:08:41 -0800

    Fixed rpi board loading

--- src/system-plugins/platform-manager/platforms/raspberrypi/board.js
+++ src/system-plugins/platform-manager/platforms/raspberrypi/board.js
@@ -32,7 +32,7 @@ BoardInterface.prototype.Compose = function (platform)
 BoardInterface.prototype.LoadInfo = function (board) 
 {
   board.info = {};
-  console.log('BOARD: Loading motherboard info from EEPROM...');
+  console.log('BOARD: Loading board info from EEPROM...');
 
   return fs.readFileAsync(path.resolve('/sys/class/i2c-adapter/i2c-1/1-0054/eeprom'))
   .then(function (data) 
@@ -49,37 +49,36 @@ BoardInterface.prototype.LoadInfo = function (board)
     console.log( "BOARD: Details:" );
     console.log( info.details );
 
-    console.log('Motherboard info: ' + JSON.stringify(info));
     board.info = info;
     board.targetBoard.info = board.info;
+
     return board;
   });
 };
 
 BoardInterface.prototype.LoadPinMap = function (board) 
 {
-  console.log( `BOARD: Loading CPU pinmap for Board[ ${board.info.boardId}]...` );
+  console.log( `BOARD: Loading CPU pinmap for Board[${board.info.boardId}]...` );
 
-  return fs.readFileAsync(path.resolve(__dirname, 'boards/' + board.info.productId + '/pinmap.json'))
+  return fs.readFileAsync(path.resolve(__dirname, 'boards/' + board.info.boardId + '/cpuPinmap.json' ) )
     .then(JSON.parse)
-    .then(function (json) 
+    .then(function( pinmap ) 
     {
-      var pinmap = json[board.info.rev];
+        console.log( "BOARD: Loaded Pinmap" );
+        console.log( "BOARD: -------------------" );
+        console.log( pinmap );
 
-      if (pinmap !== undefined) 
-      {
         board.pinmap = pinmap;
         board.targetBoard.pinmap = board.pinmap;
         return board;
-      }
     });
 };
 
-BoardInterface.prototype.LoadInterface = function (board) 
+BoardInterface.prototype.LoadInterface = function( board ) 
 {
   console.log('BOARD: Loading board interface...');
-  require('./boards/' + board.info.productId + '/setup.js')(board.targetBoard);
-  
+  require('./boards/' + board.info.boardId + '/setup.js')( board.targetBoard );
+
   return board;
 };
 

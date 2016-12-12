e4bd94167a1dda3336dcbea735fcd7a94a4fc886
Charles Cross <charles@openrov.com>
2016-12-12 15:08:29 -0800

    Updated mock interface

--- src/system-plugins/platform-manager/platforms/mock/board.js
+++ src/system-plugins/platform-manager/platforms/mock/board.js
@@ -1,34 +1,66 @@
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
-var BoardInterface = function () {
+
+var BoardInterface = function () 
+{
 };
-BoardInterface.prototype.Compose = function (platform) {
+
+BoardInterface.prototype.Compose = function (platform) 
+{
   // Temporary container used for cpu detection and info loading
   var board = { targetBoard: platform.board };
   var self = this;
-  return self.LoadInfo(board).then(self.LoadPinMap).then(self.LoadInterface);
+
+  console.log( 'BOARD: Composing Mock board interface...' );
+
+  return self.LoadInfo(board)
+    .then(self.LoadPinMap)
+    .then(self.LoadInterface);
 };
-BoardInterface.prototype.LoadInfo = function (board) {
+
+BoardInterface.prototype.LoadInfo = function (board) 
+{
   board.info = {};
-  return fs.readFileAsync(path.resolve(__dirname, 'boards/mock3000/eepromMock.json'), 'utf8').then(JSON.parse).then(function (info) {
-    board.info = info;
-    board.targetBoard.info = board.info;
+
+  console.log('BOARD: Loading board info from Mock EEPROM...');
+
+  return fs.readFileAsync(path.resolve(__dirname, 'boards/mock3000/eeprom.json'), 'utf8')
+    .then(JSON.parse)
+    .then(function (info) 
+    {
+      console.log( "BOARD: Loaded Board Info" );
+      console.log( "BOARD: -------------------" );
+      console.log( `BOARD: Product ID: ${info.productId}` );
+      console.log( `BOARD: Board ID: ${info.boardId}` );
+      console.log( "BOARD: Details:" );
+
+      board.info = info;
+      board.targetBoard.info = board.info;
+      return board;
+    });
+};
+
+BoardInterface.prototype.LoadPinMap = function (board) 
+{
+  return fs.readFileAsync(path.resolve(__dirname, 'boards/' + board.info.boardId + '/cpuPinmap.json'))
+  .then(JSON.parse)
+  .then(function (pinmap) 
+  {
+    console.log( "BOARD: Loaded Pinmap" );
+    console.log( "BOARD: -------------------" );
+    console.log( pinmap );
+
+    board.pinmap = pinmap;
+    board.targetBoard.pinmap = board.pinmap;
     return board;
   });
 };
-BoardInterface.prototype.LoadPinMap = function (board) {
-  return fs.readFileAsync(path.resolve(__dirname, 'boards/' + board.info.productId + '/pinmap.json')).then(JSON.parse).then(function (json) {
-    var pinmap = json[board.info.rev];
-    if (pinmap !== undefined) {
-      board.pinmap = pinmap;
-      board.targetBoard.pinmap = board.pinmap;
-      return board;
-    }
-  });
-};
-BoardInterface.prototype.LoadInterface = function (board) {
-  require('./boards/' + board.info.productId + '/setup.js')(board.targetBoard);
+
+BoardInterface.prototype.LoadInterface = function (board) 
+{
+  require('./boards/' + board.info.boardId + '/setup.js')(board.targetBoard);
   return board;
 };
+
 module.exports = new BoardInterface();

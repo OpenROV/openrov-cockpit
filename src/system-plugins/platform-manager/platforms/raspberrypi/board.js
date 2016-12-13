122354ea3ed91ba1f91236239d364bb51d25b074
Charles Cross <charles@openrov.com>
2016-12-13 11:07:38 -0800

    Updated mock and rpi board loaders

--- src/system-plugins/platform-manager/platforms/raspberrypi/board.js
+++ src/system-plugins/platform-manager/platforms/raspberrypi/board.js
@@ -1,87 +1,94 @@
-var Promise   = require('bluebird');
-var fs        = Promise.promisifyAll(require('fs'));
-var path      = require('path');
-var Parser    = require('binary-parser').Parser;
+const Promise   = require('bluebird');
+const fs        = Promise.promisifyAll(require('fs'));
+const path      = require('path');
+const Parser    = require('binary-parser').Parser;
 
 // TODO: Switch to debug logger
 
-// Define a parser for the board information stored on the controller board's eeprom
-var eepromParser = Parser.start().endianess('little').uint32('length').string('data', 
+class BoardInterface
 {
-    encoding: 'utf8',
-    length: 'length'
-});
-
-var BoardInterface = function () 
-{
-};
-
-BoardInterface.prototype.Compose = function (platform) 
-{
-  // Temporary container used for cpu detection and info loading
-  var board = { targetBoard: platform.board };
-  var self = this;
-
-  console.log( 'BOARD: Composing RPI board interface...' );
-
-  return self.LoadInfo(board)
-    .then(self.LoadPinMap)
-    .then(self.LoadInterface);
-};
-
-BoardInterface.prototype.LoadInfo = function (board) 
-{
-  board.info = {};
-  console.log('BOARD: Loading board info from EEPROM...');
-
-  return fs.readFileAsync(path.resolve('/sys/class/i2c-adapter/i2c-1/1-0054/eeprom'))
-  .then(function (data) 
+  constructor()
   {
-    return eepromParser.parse(data).data;
-  })
-  .then(JSON.parse)
-  .then(function (info) 
-  {
-    console.log( "BOARD: Loaded Board Info" );
-    console.log( "BOARD: -------------------" );
-    console.log( `BOARD: Product ID: ${info.productId}` );
-    console.log( `BOARD: Board ID: ${info.boardId}` );
-    console.log( "BOARD: Details:" );
-    console.log( info.details );
-
-    board.info = info;
-    board.targetBoard.info = board.info;
-
-    return board;
-  });
-};
-
-BoardInterface.prototype.LoadPinMap = function (board) 
-{
-  console.log( `BOARD: Loading CPU pinmap for Board[${board.info.boardId}]...` );
-
-  return fs.readFileAsync(path.resolve(__dirname, 'boards/' + board.info.boardId + '/cpuPinmap.json' ) )
-    .then(JSON.parse)
-    .then(function( pinmap ) 
+    // Define a parser for the board information stored on the controller board's eeprom
+    this.eepromParser = Parser.start().endianess('little').uint32('length').string('data', 
     {
-        console.log( "BOARD: Loaded Pinmap" );
-        console.log( "BOARD: -------------------" );
-        console.log( pinmap );
-
-        board.pinmap = pinmap;
-        board.targetBoard.pinmap = board.pinmap;
-        return board;
+        encoding: 'utf8',
+        length: 'length'
     });
-};
+  }
 
-BoardInterface.prototype.LoadInterface = function( board ) 
-{
-  console.log('BOARD: Loading board interface...');
-  console.log( "Loading board: " + './boards/' + board.info.boardId + '/setup.js' );
+  Compose( platform )
+  {
+    var self = this;
 
-  require('./boards/' + board.info.boardId + '/setup.js')( board.targetBoard );
+    // Temporary container used for cpu detection and info loading
+    self.board = { targetBoard: platform.board };
 
-  return board;
-};
+    console.log( 'BOARD: Composing RPI board interface...' );
+
+    return this.LoadInfo()
+      .then( () =>
+      {
+        return this.LoadPinMap();
+      })
+      .then( () =>
+      {
+        return this.LoadInterface()
+      });
+  }
+
+  LoadInfo()
+  {
+    var self = this;
+
+    console.log('BOARD: Loading board info from EEPROM...');
+
+    return fs.readFileAsync( path.resolve( '/sys/class/i2c-adapter/i2c-1/1-0054/eeprom' ) )
+      .then( (data) => 
+      {
+        return self.eepromParser.parse( data ).data;
+      })
+      .then(JSON.parse)
+      .then( (info) =>
+      {
+        console.log( "BOARD: Loaded Board Info" );
+        console.log( "BOARD: -------------------" );
+        console.log( `BOARD: Product ID: ${info.productId}` );
+        console.log( `BOARD: Board ID: ${info.boardId}` );
+        console.log( "BOARD: Details:" );
+        console.log( info.details );
+
+        self.board.info = info;
+      });
+  }
+
+  LoadPinMap() 
+  {
+    var self = this;
+
+    console.log( `BOARD: Loading CPU pinmap for Board[${self.board.info.boardId}]...` );
+
+    return fs.readFileAsync(path.resolve(__dirname, 'boards/' + self.board.info.boardId + '/cpuPinmap.json' ) )
+      .then(JSON.parse)
+      .then( (pinmap) =>
+      {
+          console.log( "BOARD: Loaded Pinmap" );
+          console.log( "BOARD: -------------------" );
+          console.log( pinmap );
+
+          self.board.pinmap = pinmap;
+      });
+  };
+
+  LoadInterface() 
+  {
+    var self = this;
+
+    self.board.targetBoard.info   = self.board.info;
+    self.board.targetBoard.pinmap = self.board.pinmap;
+
+    require( './boards/' + self.board.info.boardId + '/setup.js' )( self.board.targetBoard );
+  };
+}
 
 module.exports = new BoardInterface();

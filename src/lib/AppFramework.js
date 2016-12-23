63de55d720ae9ce1c43815b9ede32f9a631ab485
BrianAdams <brian@babastik.com>
2016-12-23 15:23:10 -0800

    Set default logging to ERROR, development to DEBUG

--- src/lib/AppFramework.js
+++ src/lib/AppFramework.js
@@ -1,4 +1,4 @@
-var logger = require('logger.js')({level:"debug",name:"main"});
+var logger = require('logger.js')({level: process.env.LOG_LEVEL || "error",name:"main"});
 
 module.exports={
     logger : logger

5033e34f4c4f7c1277c46daba84468a4c75f84e9
Charles Cross <charles@openrov.com>
2017-01-02 12:39:38 -0800

    Refactored to classes, fixed some typos, added NMC battery chemistry options, removed unused listeners, fixed duplicate battery chemistry selection bug

--- src/plugins/controllerboard2x/index.js
+++ src/plugins/controllerboard2x/index.js
@@ -1,69 +1,83 @@
-(function () {
-  var sharedFunctions = require('./public/js/telemetryToSystemPower.js');
-  var PREFERENCES_NS = 'plugins';
-  var SystemEnvionment = function SystemEnvironment(name, deps) {
-    console.log('Controllerboard2x:SystemEnvironment plugin loaded');
-    deps.globalEventLoop.on('mcu.status', function (data) {
-    });
-  };
-  var SystemPower = function SystemPower(name, deps) {
-    console.log('Controllerboard2x:SystemPower plugin loaded');
-    this.config = deps.config;
-    this.cockpit = deps.cockpit;
-    var self = this;
-    deps.globalEventLoop.on('mcu.status', function (data) {
-      var mappedPowerObject = sharedFunctions.telemetryToSystemPower(data);
-    });
-    deps.cockpit.on('plugin.systemPower.powerESCs', function (enable) {
-      if (enable){
-        deps.globalEventLoop.emit('mcu.SendCommand', 'escp(1)');
-      } else {
-        deps.globalEventLoop.emit('mcu.SendCommand', 'escp(0)');      
-      }
-    });
+var sharedFunctions = require('./public/js/telemetryToSystemPower.js');
+var PREFERENCES_NS = 'plugins';
 
-  };
-  SystemPower.prototype.getSettingSchema = function getSettingSchema() {
-    //Technically the json-editor supports "watch" that can be used
-    //to dynamically add items from an array as an ENUM to a select
-    //in the same document.  Have an issue filed as I cannot get it
-    //to work.  WORKAROUND: Manually parse the config for the Battery
-    //types and inject them in to the schema.  TODO: signal an update
-    //to the schema that is caches in several spots when a Battery
-    //is changed.
-    BatteryOptions = [
-      'TrustFire',
-      'LiFePO4'
-    ];
-    var b = this.config.preferences.get(PREFERENCES_NS);
-    if ('batteryDefintions' in b) {
-      b.batteryDefintions.batteries.forEach(function (bat) {
-        BatteryOptions.push(bat.name);
+class SystemEnvironment
+{
+    constructor(name, deps)
+    {
+      console.log('Controllerboard2x:SystemEnvironment plugin loaded');
+    }
+}
+
+class SystemPower
+{
+    constructor(name, deps)
+    {
+      console.log('Controllerboard2x:SystemPower plugin loaded');
+
+      this.config   = deps.config;
+      this.cockpit  = deps.cockpit;
+      var self      = this;
+
+      deps.cockpit.on('plugin.systemPower.powerESCs', function (enable) 
+      {
+        if (enable)
+        {
+          deps.globalEventLoop.emit('mcu.SendCommand', 'escp(1)');
+        } 
+        else 
+        {
+          deps.globalEventLoop.emit('mcu.SendCommand', 'escp(0)');      
+        }
       });
     }
-    return [{
-        'id': 'batteryDefintions',
-        'title': 'Battery Formulas',
-        'category' : 'hardware',        
-        'type': 'object',
-        'properties': {
+
+    getSettingSchema()
+    {
+      // Technically the json-editor supports "watch" that can be used
+      // to dynamically add items from an array as an ENUM to a select
+      // in the same document.  Have an issue filed as I cannot get it
+      // to work.  
+      
+      // WORKAROUND [REMOVED, SEE NOTE]: Manually parse the config for 
+      // the Battery types and inject them in to the schema.
+      // [NOTE]: Replaced with static configuration.
+
+      // TODO: signal an update to the schema that is caches in several
+      // spots when a Battery is changed.
+
+      var BatteryOptions = [
+        'TrustFire',
+        'LiFePO4',
+        'NMC'
+      ];
+
+      return [{
+        'id':       'batteryDefinitions',
+        'title':    'Battery Formulas',
+        'category': 'hardware',        
+        'type':     'object',
+
+        'properties': 
+        {
           'batteries': {
-            'id': 'batteries',
+            'id':   'batteries',
             'type': 'array',
+
             'items': {
-              'id': '0',
+              'id':   '0',
               'type': 'object',
               'properties': {
                 'name': {
-                  'id': 'name',
+                  'id':   'name',
                   'type': 'string'
                 },
                 'minVoltage': {
-                  'id': 'minVoltage',
+                  'id':   'minVoltage',
                   'type': 'integer'
                 },
                 'maxVoltage': {
-                  'id': 'maxVoltage',
+                  'id':   'maxVoltage',
                   'type': 'integer'
                 }
               },
@@ -73,8 +87,8 @@
                 'maxVoltage'
               ]
             },
-            'required': ['0'],
-            'default': [
+            'default': 
+            [
               {
                 'name': 'TrustFire',
                 'minVoltage': 8,
@@ -88,57 +102,52 @@
               {
                 'name': 'NMC',
                 'minVoltage': 7,
-                'maxVoltage': 10.8
+                'maxVoltage': 10
               }
             ]
           },
-          'selectedBattery': {
-            'id': 'selectedBattery',
-            'type': 'string',
+          'selectedBattery': 
+          {
+            'id':       'selectedBattery',
+            'type':     'string',
             'enum': BatteryOptions,
-            'default': 'LiFePO4'
+            'default':  'LiFePO4'  
           }
         },
-        'required': [
+        'required': 
+        [
           'batteries',
           'selectedBattery'
         ]
       }];
-  };
-  /*
-,
-"selectedBattery" : {
-  "id" : "selectedBattery",
-  "type" : "string",
-  "watch" : {
-    "batteryEnum" : "batteries"
-  },
-  "enumSource" : [{
-    "source" : "batteryEnum",
-    "value" : "{{item.name}}"
-  }]
-}
-*/
-  var Controllerboard2x = function Controllerboard2x(name, deps) {
-    console.log('Controllerboard2x plugin loaded');
-    deps.globalEventLoop.on('mcu.status', function (data) {
-    });
-  };
-  Controllerboard2x.prototype.getSettingSchema = function getSettingSchema() {
-    var item = this.systempower.getSettingSchema();
-    return item;
-  };
-  module.exports = function (name, deps)
-   {
-    if( process.env.PRODUCTID == "trident" )
+    }
+};
+
+// Controller Board 2X Plugin
+class Controllerboard2x
+{
+    constructor(name, deps)
     {
-        console.log( "Not supported on trident" );
-        return {};
+      console.log('Controllerboard2x plugin loaded');
+
+      this.systempower        = new SystemPower(name, deps);
+      this.systemenvironment  = new SystemEnvironment(name, deps);
     }
 
-    var result = new Controllerboard2x(name, deps);
-    result.systempower = new SystemPower(name, deps);
-    result.systemenvionment = new SystemEnvionment(name, deps);
-    return result;
-  };
-}());
+    getSettingSchema() 
+    {
+      var item = this.systempower.getSettingSchema();
+      return item;
+    }
+}
+
+module.exports = function (name, deps)
+{
+  if( process.env.PRODUCTID == "trident" )
+  {
+      console.log( "Not supported on trident" );
+      return {};
+  }
+
+  return new Controllerboard2x(name, deps);
+};

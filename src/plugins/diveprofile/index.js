59ee068d41ea9fcf7661682eb969586b09060fff
Charles Cross <charles@openrov.com>
2017-01-02 22:08:15 -0800

    Reworked diveprofile plugin and added back in functionality for zeroing depth and setting the water type. Added reporting for water temperature and pressure

--- src/plugins/diveprofile/index.js
+++ src/plugins/diveprofile/index.js
@@ -1,66 +1,200 @@
-function diveprofile(name, deps) {
-  console.log('This is where DiveProfile plugin.');
-  //instance variables
-  this.cockpit = deps.cockpit;
-  this.global = deps.globalEventLoop;
-  this.deps = deps;
+const Periodic = require( 'Periodic' );
+const Listener = require( 'Listener' );
+
+// Encoding helper functions
+function encode( floatIn )
+{
+    return parseInt( floatIn * 1000 );
 }
-diveprofile.prototype.start = function start() {
-  var self = this;
-  this.deps.globalEventLoop.on('mcu.status', function (data) {
-    var watertype;
-    if ('dtwa' in data) {
-      watertype = {
-        raw: data.dtwa,
-        watertype: data.dtwa.toString() == '1' ? 'salt' : 'fresh'
-      };
+
+function decode( intIn )
+{
+    return ( intIn * 0.001 );
+}
+
+class DiveProfile
+{
+  constructor(name, deps) 
+  {
+    console.log( 'DiveProfile plugin loaded' );
+    var self = this;
+
+    // Comm buses
+    this.globalBus  = deps.globalEventLoop;
+    this.cockpitBus = deps.cockpit;
+
+    // Plugin settings
+    this.settings = {};
+
+    // Target settings
+    this.targetWaterType  = 0;      // 0 = Fresh, 1 = Salt
+
+    // MCU's reported settings
+    this.mcuWaterType     = NaN;
+    this.zeroDepthAck     = false;
+
+    // State information
+    this.depth        = 0;    // meters
+    this.pressure     = 0;    // kPa
+    this.temperature  = 0;    // celsius
+
+    this.SyncSettings = new Periodic( 100, "timeout", function()
+    {
+        let synced = true;
+
+        // Send water type until synced
+        if( self.mcuWaterType !== self.targetWaterType )
+        {
+            synced = false;
+
+            // Emit command to mcu
+            var command = 'depth_water(' + self.targetWaterType + ')';
+            self.globalBus.emit( 'mcu.SendCommand', command );
+        }
+
+        // TODO: Max Attempts
+        if( synced )
+        {
+            // Successfully synced
+            self.SyncSettings.stop();
+
+            // Enable API
+            self.listeners.zeroDepth.enable();
+        }
+    });
+
+    // Periodic function that commands the MCU to zero the depth value of the depth sensor
+    this.SyncZeroDepth = new Periodic( 100, "timeout", () =>
+    {
+        // TODO: Max Attempts
+        if( self.zeroDepthAck !== true )
+        {
+            // Emit command to mcu
+            var command = 'depth_zero()';
+            self.globalBus.emit( 'mcu.SendCommand', command );
+        }
+        else
+        {
+            // Stop syncing
+            self.SyncZeroDepth.stop();
+        }
+    });
+
+    this.listeners = 
+    {
+        settings: new Listener( this.globalBus, 'settings-change.diveProfile', true, ( settings ) =>
+        {
+            // Apply settings
+            self.settings = settings.diveProfile;
+
+            // Set target water type
+            if( self.settings.waterType == "Freshwater" )
+            {
+              self.targetWaterType = 0;
+            }
+            else if( self.settings.waterType == "Saltwater" )
+            {
+              self.targetWaterType = 1;
+            }
+
+            // Enable MCU Status listener
+            self.listeners.mcuStatus.enable();
+
+            // Start syncing the current settings with the MCU
+            self.SyncSettings.start();
+        }),
+
+        mcuStatus: new Listener( this.globalBus, 'mcu.status', false, ( data ) =>
+        {
+            // Depth
+            if( 'depth_d' in data ) 
+            {
+                self.depth = decode( parseInt( data.depth_d ) );
+                self.globalBus.emit( "plugin.diveprofile.depth", self.depth );
+            }
+
+            // Pressure
+            if( 'depth_p' in data ) 
+            {
+                self.pressure = decode( parseInt( data.depth_p ) );
+                self.globalBus.emit( "plugin.diveprofile.pressure", self.pressure );
+            }
+
+            // Temperature
+            if( 'depth_t' in data ) 
+            {
+                self.temperature = decode( parseInt( data.depth_t ) );
+                self.globalBus.emit( "plugin.diveprofile.temp", self.temperature );
+            }
+    
+            // Water type
+            if( 'depth_water' in data ) 
+            {
+                self.mcuWaterType = parseInt( data.depth_water );
+            }
+
+            // Depth zero ack
+            if( 'depth_zero' in data ) 
+            {
+                if( data.depth_zero == "ack" )
+                {
+                    // Done syncing
+                    self.zeroDepthAck = true;
+                }
+            }
+        }),
+
+        zeroDepth: new Listener( this.cockpitBus, 'plugin.diveprofile.zeroDepth', false, () =>
+        {
+            // Zero the depth value by using the current value as the offset
+            self.setZeroDepth();
+        })
     }
-    if ('settings' in data) {
-      if (data.settings.water_type) {
-        watertype = {
-          raw: data.settings.water_type,
-          watertype: data.settings.water_type.toString() == '1' ? 'salt' : 'fresh'
-        };
-      }
-    }
-    if (watertype !== undefined) {
-      self.deps.cockpit.emit('plugin.diveprofile.watertype', watertype);
-      self.deps.config.preferences.set('plugin:diveprofile:water_type', watertype);
-    }
-  });
-  this.deps.cockpit.on('plugin.diveprofile.watertype.toggle', function () {
-    self.deps.globalEventLoop.emit('mcu.SendCommand', 'dtwa()');
-  });
-  self.global.withHistory.on('settings-change.diveprofile', function (data) {
-    var settings = data.diveprofile;  //TODO: Replumb the arduino APIs to explicilty set the water type
-                                      //settings.['water-type']
-  });
-  this.deps.cockpit.on('plugin.diveprofile.watertype.set', function (type) {
-    var value = type == 'salt' ? 1 : 0;
-    self.deps.globalEventLoop.emit('mcu.SendCommand', 'dswa(' + value + ')');
-  });
-  this.deps.cockpit.on('plugin.diveprofile.depth.zero', function () {
-    self.deps.globalEventLoop.emit('mcu.SendCommand', 'dzer()');
-  });
-};
-diveprofile.prototype.getSettingSchema = function getSettingSchema() {
-  return [{
+  }
+
+  setZeroDepth()
+  {
+    // Reset the ack and start syncing state
+    this.zeroDepthAck = false;
+    this.SyncZeroDepth.start();
+  }
+
+  start()
+  {
+    this.listeners.settings.enable();
+  }
+
+  stop()
+  {
+    self.SyncSettings.stop();
+    self.SyncZeroDepth.stop();
+    this.listeners.settings.disable();
+    this.listeners.mcuStatus.disable();
+    this.listeners.zeroDepth.disable();
+  }
+
+  getSettingSchema()
+  {
+    return [{
       'title': 'Dive Profile',
-      'id': 'diveprofile',
+      'id': 'diveProfile',
       'type': 'object',
       'properties': {
-        'water-type': {
+        'waterType': {
           'type': 'string',
           'enum': [
             'salt',
             'fresh'
           ],
-          'title': 'Water type',
+          'title': 'Water Type',
           'default': 'fresh'
         }
       }
     }];
-};
-module.exports = function (name, deps) {
-  return new diveprofile(name, deps);
+  }
+}
+
+module.exports = function (name, deps) 
+{
+  return new DiveProfile(name, deps);
 };
